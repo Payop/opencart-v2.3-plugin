820822495e50b190322367c4466417fe0888e41d
DenysVolynets <denvolynets216@gmail.com>
2019-12-23 12:58:52 +0200

    v2 init

--- upload/catalog/controller/extension/payment/payop.php
+++ upload/catalog/controller/extension/payment/payop.php
@@ -17,29 +17,29 @@ class ControllerExtensionPaymentPayop extends Controller
         $orderData = [];
         $orderData['publicKey'] = $this->config->get('payop_public');
         $orderData['order'] = [];
-        $orderData['order']['id'] = $this->session->data['order_id'];
+        $orderData['order']['id'] = strval($this->session->data['order_id']);
         $orderData['order']['amount'] = number_format($this->currency->format($order_info['total'], $order_info['currency_code'], '', false), 4);
         $orderData['order']['currency'] = $order_info['currency_code'];
         $orderData['order']['description'] = 'Payment order #' . $this->session->data['order_id'];
-        $orderData['customer'] = [];
-        $orderData['customer']['email'] = $order_info['email'];
-        $orderData['customer']['name'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
-        $orderData['customer']['phone'] = $order_info['telephone'];
+        $orderData['order']['items'] = [];
+        $orderData['payer'] = [];
+        $orderData['payer']['email'] = $order_info['email'];
+        $orderData['payer']['name'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
+        $orderData['payer']['phone'] = $order_info['telephone'];
         $orderData['language'] = ($this->session->data['language'] == 'ru_ru') ? 'ru' : 'en';
         $orderData['resultUrl'] = $this->url->link('checkout/success');
-        $orderData['failUrl'] = $this->url->link('checkout/failure');
+        $orderData['failPath'] = $this->url->link('checkout/failure');
 
         /** generate signature */
-        $orderSet = ['id' => $this->session->data['order_id'], 'amount' => number_format($this->currency->format($order_info['total'], $order_info['currency_code'], '', false), 4), 'currency' => $order_info['currency_code']];
-        ksort($orderSet, SORT_STRING);
-        $dataSet = array_values($orderSet);
-        array_push($dataSet, $this->config->get('payop_secret'));
-        $orderData['signature'] = hash('sha256', implode(':', $dataSet));
-
-        $payUrl = $this->apiRequest($orderData);
-        $data['action'] = $payUrl['data']['redirectUrl'];
+        $orderData['signature'] = $this->generate_signature(
+            $this->session->data['order_id'],
+            number_format($this->currency->format($order_info['total'],
+            $order_info['currency_code'], '', false), 4), $order_info['currency_code'],
+            $this->config->get('payop_secret')
+        );
+        $invoice_id = $this->apiRequest($orderData);
+        $data['action'] = "https://payop.com/{$orderData['language']}/payment/invoice-preprocessing/{$invoice_id}";
         return $this->load->view('extension/payment/payop', $data);
-
     }
 
     /**
@@ -48,17 +48,26 @@ class ControllerExtensionPaymentPayop extends Controller
      */
     private function apiRequest($orderData)
     {
-        $apiUrl = 'https://PayOp.com/api/v1.1/payments/payment';
+        $apiUrl = 'https://payop.com/v1/invoices/create';
         $data = json_encode($orderData);
+
         $ch = curl_init($apiUrl);
+        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $headers = substr($result, 0, $header_size);
+        $headers = explode("\r\n", $headers);
+        $invoice_identifier = preg_grep("/^identifier/", $headers);
+        $invoice_identifier = implode(',' , $invoice_identifier);
+        $invoice_identifier = substr($invoice_identifier, strrpos($invoice_identifier, ':')+2);
         curl_close($ch);
-        return json_decode($result, true);
+        return $invoice_identifier;
     }
 
     /**
@@ -66,20 +75,74 @@ class ControllerExtensionPaymentPayop extends Controller
      */
     public function callback()
     {
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $callback = json_decode(file_get_contents('php://input'));
+            $callback = json_encode($callback);
+            $callback = json_decode($callback, false);
             if (is_object($callback)) {
-                $sign_string = $callback->amount . ':' . $callback->currency . ':' . $callback->orderId . ':' . $callback->status . ':' . $this->config->get('payop_secret');
-                if ($callback->signature == hash('sha256', $sign_string)) {
-                    $this->load->model('checkout/order');
-                    if ($callback->status === 'success')
-                        $this->model_checkout_order->addOrderHistory($callback->orderId, $this->config->get('payop_order_status_id'));
-                    else if ($callback->status === 'error')
-                        $this->model_checkout_order->addOrderHistory($callback->orderId, $this->config->get('payop_failed_status_id'));
+                if(isset($callback->invoice)) {
+                    if ($this->callback_check($callback) === 'valid'){
+                        $this->load->model('checkout/order');
+                        if($callback->transaction->state === 2) {
+                            $this->model_checkout_order->addOrderHistory($callback->transaction->order->id, $this->config->get('payop_order_status_id'));
+                        } elseif ($callback->transaction->state === 3 or $callback->transaction->state === 5) {
+                            $this->model_checkout_order->addOrderHistory($callback->transaction->order->id, $this->config->get('payop_failed_status_id'));
+                        }
+                    } else {
+                        $this->log->write('Error callback: '. $this->callback_check($callback));
+                    }
                 } else {
-                    $this->log->write('Error callback!');
+                    $signature = $this->generate_signature($callback->orderId, $callback->amount, $callback->currency, $this->config->get('payop_secret'));
+                    if ($callback->signature == $signature) {
+                        $this->load->model('checkout/order');
+                        if ($callback->status === 'success')
+                            $this->model_checkout_order->addOrderHistory($callback->orderId, $this->config->get('payop_order_status_id'));
+                        else if ($callback->status === 'error')
+                            $this->model_checkout_order->addOrderHistory($callback->orderId, $this->config->get('payop_failed_status_id'));
+                    } else {
+                        $this->log->write('Error callback!');
+                    }
                 }
             }
         }
+    }
+
+    /**
+     * @return boolean
+     * @param $callback object
+     */
+    private function callback_check($callback)
+    {
+        $invoiceId = !empty($callback->invoice->id) ? $callback->invoice->id : null;
+        $txid = !empty($callback->invoice->txid) ? $callback->invoice->txid : null;
+        $orderId = !empty($callback->transaction->order->id) ? $callback->transaction->order->id : null;
+        $state = !empty($callback->transaction->state) ? $callback->transaction->state : null;
+
+        if (!$invoiceId) {
+            return 'Empty invoice id';
+        }
+        if (!$txid) {
+            return 'Empty transaction id';
+        }
+        if (!$orderId) {
+            return 'Empty order id';
+        }
+        if (!(1 <= $state && $state <= 5)) {
+            return 'State is not valid';
+        }
+        return 'valid';
+    }
+
+    /**
+     * @return string
+     */
+    private function generate_signature($orderId, $amount, $currency, $secretKey)
+    {
+        $sign_str = ['id' => $orderId, 'amount' => $amount, 'currency' => $currency];
+        ksort($sign_str, SORT_STRING);
+        $sign_data = array_values($sign_str);
+        array_push($sign_data, $secretKey);
+        return hash('sha256', implode(':', $sign_data));
     }
 }
